672026643392e12d699bba02e7efe1a84ef60743
Matheus <matheus@geekbusinesslab.com.br>
2022-03-02 22:20:05 -0300

    adjusting project created by slidy

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 
 class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Slidy',
       theme: ThemeData(primarySwatch: Colors.blue),
-    ).modular();
+    );
   }
 }
